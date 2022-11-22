6d98b3234845db384df8feffbdb56fb3b0ce5f91
Paul <paulnyoiken@gmail.com>
2022-11-22 22:22:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+
+const saturdayFun = (activity ='roller-skate') => `This Saturday, I want to ${activity}!`
+
+saturdayFun("bathe my dog")
+
+const mondayWork = (activity = 'go to the office') => `This Monday, I will ${activity}.`
+
+const wrapAdjective = (paramOut = "*") => {
+    return function (paramIn = "special") {
+        return (`You are ${paramOut}${paramIn}${paramOut}!`)
+    }
+}
+
+wrapAdjective("||")("a dedicated programmer")
